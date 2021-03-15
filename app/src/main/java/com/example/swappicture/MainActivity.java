f67845e363eaf808734cc481b937f9b5d4ab2d46
serg0 <serg.01@bk.ru>
2021-03-16 02:23:48 +0300

    fix code style

--- app/src/main/java/com/example/swappicture/MainActivity.java
+++ app/src/main/java/com/example/swappicture/MainActivity.java
@@ -9,37 +9,39 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn2 ;
-    Button btn1 ;
+    private Button btn2 ;
+    private Button btn1 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int diapozon = (int) (Math.random()*100)+1;
-        String chislo = String.valueOf(diapozon);
-        String str = getString(R.string.text)+chislo;
-        TextView textView = findViewById(R.id.textView);
+        int range = (int) (Math.random()*100)+1;
+        String number = String.valueOf(range);
+        String str = getString(R.string.text)+number;
+        TextView textView = findViewById(R.id.text_view);
         textView.setText(str);
-        initNext();
-        initDown();
+        init();
+        clickListener();
 
     }
-    private void initNext(){
-        btn1 =  findViewById(R.id.button2);
+    private void init(){
+        btn1 =  findViewById(R.id.button_two);
+        btn2 =  findViewById(R.id.button_one);
+
+    }
+    private void clickListener(){
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(intent);
-            }
-        });
-    }
-    private void  initDown(){
-        btn2 =  findViewById(R.id.button1);
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
             }
         });
     }
